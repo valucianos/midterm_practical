dd7402653a475e9e9e6beb8c25d1f031a4e9c26c
valucianos <70271776+valucianos@users.noreply.github.com>
2020-10-12 20:50:13 -0400

    Add files via upload

---
+++ script.js
@@ -0,0 +1,42 @@
+var wrapperEle = document.body.querySelector(".wrapper");
+var list = [
+  {
+    name: "Harry",
+    age: 25
+  },
+  {
+    name: "Larry",
+    age: 15
+  },
+  {
+    name: "Bert",
+    age: 45
+  },
+  {
+    name: "Jerry",
+    age: 35
+  },
+  {
+    name: "Berry",
+    age: 5
+  },
+  {
+    name: "Bart",
+    age: 55
+  }
+];
+for (var i = 0; i < list.length; i++) {
+  var listEle = document.createElement("div");
+  listEle.innerHTML = list[i].name;
+  wrapperEle.appendChild(listEle);
+  if (list[i].age >= 40) {
+    wrapperEle.appendChild(listEle).style.color = "red";
+  }
+}
+var i = 0;
+function buttonClick() {
+  document.body.querySelector(".wrapper").innerHTML = ++i;
+}
+document.body.querySelector(".button").addEventListener("click", function () {
+  buttonClick();
+})
